7431832c1f80efba35687ee64a5739aa5f46f901
Robin Pettersen <ssbmtonberry@gmail.com>
2020-12-30 17:07:08 +0100

    Minor decompression code improvement + cleanup

--- pocketlzma.hpp
+++ pocketlzma.hpp
@@ -6736,6 +6736,11 @@ namespace plz
 		ErrorArchive = SZ_ERROR_ARCHIVE, //16
 		ErrorNoArchive = SZ_ERROR_NO_ARCHIVE, //17
 
+		/*!
+		 * When attempting to decompress, but size info is missing in header.
+		 */
+		MissingSizeInfoInHeader = 100,
+
 		UndefinedError = 999
 	};
 }
@@ -7165,7 +7170,9 @@ namespace plz
 			PocketLzma() = default;
 			explicit PocketLzma(const Settings &settings) : m_settings {settings} {};
 			inline void setSettings(const Settings &settings);
+
 			inline StatusCode compress(const std::vector<uint8_t> &input, std::vector<uint8_t> &output);
+			inline StatusCode decompress(const std::vector<uint8_t> &input, std::vector<uint8_t> &output);
 
 		private:
 			Settings m_settings {};
@@ -7183,18 +7190,56 @@ namespace plz
 	{
 		m_settings.validate();
 		size_t propsSize = LZMA_PROPS_SIZE;
-		//uint8_t propsEncoded[propsSize + 8];
 		uint8_t propsEncoded[propsSize];
-		size_t outSize = input.size();
+
+		size_t outSize = input.size() + (input.size() / 3) + 128;
 		uint8_t out[outSize];
 
 		int rc = plz::c::LzmaCompress(out, &outSize, &input[0], input.size(), propsEncoded, &propsSize, m_settings.level, m_settings.dictionarySize,
 							 m_settings.literalContextBits,m_settings.literalPositionBits,m_settings.positionBits,m_settings.fastBytes,1);
 
-		std::vector<uint8_t> buffer(out, out + outSize);
-		output.insert(output.end(), buffer.begin(), buffer.end());
+		StatusCode status = static_cast<StatusCode>(rc);
+		if(status == StatusCode::Ok)
+		{
+			std::vector<uint8_t> sizeBits;
+			for (int i = 0; i < 8; i++)
+				sizeBits.push_back((input.size() >> (i * 8)) & 0xFF);
+
+			output.insert(output.end(), propsEncoded, propsEncoded + propsSize); // Property header
+			output.insert(output.end(), sizeBits.begin(), sizeBits.end());           // Add decompress size information
+			output.insert(output.end(), out, out + outSize);                     // Data
+		}
+
+		return status;
+	}
+
+	StatusCode PocketLzma::decompress(const std::vector<uint8_t> &input, std::vector<uint8_t> &output)
+	{
+		size_t propsSize = LZMA_PROPS_SIZE + 8; //header + decompress_size
+		size_t size = 0;
+		bool sizeInfoMissing = true; //True until proven otherwise
+
+		for (int i = 0; i < 8; i++)
+		{
+			uint8_t value = input[LZMA_PROPS_SIZE + i];
+			if(value != 0xFF)
+				sizeInfoMissing = false;
+
+			size |= (value << (i * 8));
+		}
+
+		if(sizeInfoMissing)
+			return StatusCode::MissingSizeInfoInHeader;
+
+		uint8_t out[size];
+		size_t outSize = size;
+
+		size_t inSize = input.size() - propsSize;
+		int rc = plz::c::LzmaUncompress(out, &outSize, &input[propsSize], &inSize, &input[0], LZMA_PROPS_SIZE);
 
 		StatusCode status = static_cast<StatusCode>(rc);
+
+		output.insert(output.end(), out, out + outSize);
 
 		return status;
 	}
--- src/pocketlzma_class.hpp
+++ src/pocketlzma_class.hpp
@@ -33,23 +33,14 @@ namespace plz
     {
         m_settings.validate();
         size_t propsSize = LZMA_PROPS_SIZE;
-        //uint8_t propsEncoded[propsSize + 8];
         uint8_t propsEncoded[propsSize];
 
         size_t outSize = input.size() + (input.size() / 3) + 128;
         uint8_t out[outSize];
 
-        //int rc = plz::c::LzmaCompress(out, &outSize, &input[0], input.size(), propsEncoded, &propsSize, m_settings.level, m_settings.dictionarySize,
-        //                     m_settings.literalContextBits,m_settings.literalPositionBits,m_settings.positionBits,m_settings.fastBytes,1);
-        //uint8_t *out;
-        //size_t outSize;
         int rc = plz::c::LzmaCompress(out, &outSize, &input[0], input.size(), propsEncoded, &propsSize, m_settings.level, m_settings.dictionarySize,
                              m_settings.literalContextBits,m_settings.literalPositionBits,m_settings.positionBits,m_settings.fastBytes,1);
 
-        //std::vector<uint8_t> buffer(out, out + outSize);
-        //output.insert(output.end(), buffer.begin(), buffer.end());
-
-        //output.resize(outSize);
         StatusCode status = static_cast<StatusCode>(rc);
         if(status == StatusCode::Ok)
         {
@@ -57,61 +48,41 @@ namespace plz
             for (int i = 0; i < 8; i++)
                 sizeBits.push_back((input.size() >> (i * 8)) & 0xFF);
 
-            output.insert(output.end(), propsEncoded, propsEncoded + propsSize);
-            output.insert(output.end(), sizeBits.begin(), sizeBits.end()); //Add decompress size information
-            output.insert(output.end(), out, out + outSize);
+            output.insert(output.end(), propsEncoded, propsEncoded + propsSize); // Property header
+            output.insert(output.end(), sizeBits.begin(), sizeBits.end());           // Add decompress size information
+            output.insert(output.end(), out, out + outSize);                     // Data
         }
-
-        //delete[] out; //RBP: Make sure memory is freed
-        //free(out);
 
         return status;
     }
 
     StatusCode PocketLzma::decompress(const std::vector<uint8_t> &input, std::vector<uint8_t> &output)
     {
-        size_t propsSize = LZMA_PROPS_SIZE + 8; //header + decompress size
-
-        //size_t inSize = input.size();
-        //uint8_t out[1 << 16];
-        //size_t outSize = 1 << 16;
-        //output.resize(5000000);
-
-
-        //Extract size
-
-        size_t size = 0;//5000000;
+        size_t propsSize = LZMA_PROPS_SIZE + 8; //header + decompress_size
+        size_t size = 0;
         bool sizeInfoMissing = true; //True until proven otherwise
 
         for (int i = 0; i < 8; i++)
         {
-            uint8_t value = input[5 + i];
+            uint8_t value = input[LZMA_PROPS_SIZE + i];
             if(value != 0xFF)
                 sizeInfoMissing = false;
 
-            size |= (input[5 + i] << (i * 8));
+            size |= (value << (i * 8));
         }
 
         if(sizeInfoMissing)
             return StatusCode::MissingSizeInfoInHeader;
 
-        //uint8_t *out;
-        //std::unique_ptr<uint8_t> out = std::unique_ptr<uint8_t>();
-        std::unique_ptr<uint8_t[]> out = std::unique_ptr<uint8_t[]>(new uint8_t[size]);
-        //size_t outSize;
+        uint8_t out[size];
         size_t outSize = size;
 
         size_t inSize = input.size() - propsSize;
-        //int rc = plz::c::LzmaUncompress(out, &outSize, &input[0], &inSize, propsEncoded, propsSize);
-        int rc = plz::c::LzmaUncompress(out.get(), &outSize, &input[propsSize], &inSize, &input[0], LZMA_PROPS_SIZE);
-        //int rc = plz::c::LzmaUncompress(out, &outSize, &input[propsSize], &inSize, &input[0], LZMA_PROPS_SIZE);
+        int rc = plz::c::LzmaUncompress(out, &outSize, &input[propsSize], &inSize, &input[0], LZMA_PROPS_SIZE);
 
         StatusCode status = static_cast<StatusCode>(rc);
 
-        output.insert(output.end(), out.get(), out.get() + outSize);
-        //output.insert(output.end(), out, out + outSize);
-        //free(out); //RBP: Make sure memory is freed
-
+        output.insert(output.end(), out, out + outSize);
 
         return status;
     }
