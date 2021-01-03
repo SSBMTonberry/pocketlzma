46c5ca4071dee47025acd95d01c71032e53bef74
Robin Pettersen <ssbmtonberry@gmail.com>
2021-01-03 16:29:35 +0100

    Added osx build. Works fine.

---
+++ tests_program/osx/script.sh
@@ -0,0 +1,7 @@
+./pocketlzma -d ./../files/compressed.lzma ./../files/decompressed.json
+./pocketlzma -c ./../files/file.json
+./pocketlzma -cf+ ./../files/file.json ./../files/fastest.lzma
+./pocketlzma -cf ./../files/file.json ./../files/fast.lzma
+./pocketlzma -cg ./../files/file.json ./../files/good.lzma
+./pocketlzma -cb ./../files/file.json ./../files/best.lzma
+./pocketlzma -d ./../files/compressed.lzma
